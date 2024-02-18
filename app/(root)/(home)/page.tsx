09f872f9e57cb7dc094c6426d04621e400768e97
Mreza <seyyedi98@outlook.com>
2024-02-18 19:49:11 +0330

    add auth using clerk

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,10 @@
-import React from "react";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="h-screen">
+      <UserButton />
+      Home
+    </div>
+  );
 }
